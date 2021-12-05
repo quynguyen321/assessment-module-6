a61d6490dc9e6119e9b8b6b9bc89f55c2cf30f68
quynguyen321 <quy.ybu@gmail.com>
2021-12-05 16:06:21 -0600

    done

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,12 +17,27 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+});
+
+
+test('draw btn', async () => {
+
+    let button = await (await driver).findElement(By.id('draw'));
+    await button.click();   
+    await driver.sleep(2000) 
+});
+
+test('see all bot', async () => {
+
+    let button = await (await driver).findElement(By.id('see-all'));
+    
+    await button.click();    
+});
+test ("add to duo button", async () => {
+    await driver.sleep(2000)
+
+    const addBtn = await driver.findElement(By.xpath("(//section/div)[2]"))
 })
 
-    test('Clicking ', async () => {
-        const box = await driver.findElement(By.id('choices'))
-        await box.click()
-    
-     
-        await driver.sleep(2000)
-    })
+
+
